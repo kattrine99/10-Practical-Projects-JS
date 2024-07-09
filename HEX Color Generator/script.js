746af319e1f14226be169c3ff57279d070a19231
Ekaterina <kattren990703@gmail.com>
2024-07-09 13:13:21 +0500

    Updated project with color generator

--- HEX Color Generator/script.js
+++ HEX Color Generator/script.js
@@ -11,13 +11,45 @@ function getRandomColor(){
     }
     return color;
 }
+function darkenColor(color, amount) {
+    let usePound = false;
+
+    if (color[0] === "#") {
+        color = color.slice(1);
+        usePound = true;
+    }
+
+    let num = parseInt(color, 16);
+
+    let r = (num >> 16) - amount;
+    let g = ((num >> 8) & 0x00FF) - amount;
+    let b = (num & 0x0000FF) - amount;
+
+    r = r < 0 ? 0 : r;
+    g = g < 0 ? 0 : g;
+    b = b < 0 ? 0 : b;
+
+    let darkerColor = (usePound ? "#" : "") + (r << 16 | g << 8 | b).toString(16).padStart(6, '0').toUpperCase();
+    
+    return darkerColor;
+}
 
 function updateColor(){
     const colorBox = document.getElementById("color-box");
+    const colorBox1 = document.getElementById("color-box1");
+    const colorBox2 = document.getElementById("color-box2");
     const colorText = document.getElementById("colorText");
+    const colorText1 = document.getElementById("colorText1");
+    const colorText2 = document.getElementById("colorText2");
+
+
 
     const randomColor = getRandomColor();
-
     colorBox.style.backgroundColor = randomColor;
     colorText.textContent = randomColor;
+    
+    colorBox1.style.backgroundColor = darkenColor(randomColor,32);
+    colorText1.textContent = darkenColor(randomColor,32);
+    colorBox2.style.backgroundColor = darkenColor(randomColor,64);
+    colorText2.textContent = darkenColor(randomColor,64);
 }
